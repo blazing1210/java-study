42330e63618f44fe13ba149f285ec2b33f379020
blazing1210 <blazing1210@naver.com>
2021-10-13 12:30:38 +0900

    two java

---
+++ CH02_Variables/src/integer/Arithmethic.java
@@ -0,0 +1,23 @@
+package integer;
+
+public class Arithmethic {
+
+	public static void main(String[] args) {
+		//산술연산
+		int x;
+		System.out.println(7+3);
+		
+		System.out.println(9/3);
+		System.out.println(10/3);
+		
+		System.out.println(10.0-3.0);
+		System.out.println(9/4.0);
+		
+		System.out.println(3*4);
+		System.out.println(3*4.0);
+		
+		System.out.println(3-4);
+
+	}
+
+}
